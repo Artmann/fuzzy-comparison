9c27f35aff429b21c3231389a0b9a9676b2c44c2
Christoffer Artmann <Artgaard@gmail.com>
2020-06-07 00:37:22 +0200

    refactor: Migrate to TypeScript.

--- src/compare.spec.ts
+++ src/compare.spec.ts
@@ -38,21 +38,6 @@ describe('compare', () => {
     });
   });
 
-  describe('handles null values', () => {
-    it('returns true if both values are falsy', () => {
-      expect(compare(null, null)).toBeTruthy();
-      expect(compare(null, undefined)).toBeTruthy();
-      expect(compare(undefined, null)).toBeTruthy();
-    });
-
-    it('returns false if only one value is falsy', () => {
-      expect(compare(null, 'foobar')).toBeFalsy();
-      expect(compare(undefined, 'foobar')).toBeFalsy();
-      expect(compare('foobar', null)).toBeFalsy();
-      expect(compare('foobar', undefined)).toBeFalsy();
-    });
-  });
-
   it('has an adjustable threshold', () => {
     expect(compare('foo', 'faa')).toBeTruthy();
     expect(compare('foo', 'foo', { threshold: 0 })).toBeTruthy();
